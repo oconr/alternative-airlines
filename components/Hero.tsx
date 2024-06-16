5a085e537372deb5072ff81b08f9b23fb92fcc9f
Ryan O'Connor <github@oconr.uk>
2024-06-16 14:54:08 +0100

    Added 'Search flights' button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -87,6 +87,7 @@ export default function Hero() {
                   <PassengerInput />
                 </div>
               </div>
+              <button>Search flights</button>
             </div>
           </form>
         </div>
